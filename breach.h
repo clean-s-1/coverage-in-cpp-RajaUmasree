1264e28162d528ba8b89fe195d97193aeff9a6b2
RajaUmasree <102411766+RajaUmasree@users.noreply.github.com>
2022-07-18 15:33:07 +0530

    Updated breach.h

--- breach.h
+++ breach.h
@@ -1,11 +1,44 @@
 #pragma once
+
 #include <stdio.h>
 #include <string>
 #include "typewise-alert.h"
 
+class Coolingtype {
+    private:
+         double lowerLimit;
+         double upperLimit;
+    public:
+        Coolingtype(int lowlimit, int highlimit) : lowerLimit{lowlimit}, upperLimit{highlimit} {}
+        
+        double getlowlimit() {
+            return lowerLimit;
+        }
+        double gethighlimit() {
+            return upperLimit;
+        }
+};
+
+class Passivecooling: public Coolingtype {
+    public:
+        passivecooling() : Coolingtype(0,20) {}
+};
+
+class Activecooling: public Coolingtype {
+    public:
+        activecooling() : Coolingtype(0,20) {}
+};
+
+class Normalcooling: public Coolingtype {
+    public:
+        passivecooling() : Coolingtype(0,20) {}
+};
+
 
 class Breach {
+  Coolingtype *coolingtype= nullptr;
   public:
+    
     BreachType inferBreach(double value, double lowerLimit, double upperLimit);
-    BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC); 
+    BreachType classifyTemperatureBreach(Coolingtype* coolingtypeobj, double temperatureInC); 
 };
